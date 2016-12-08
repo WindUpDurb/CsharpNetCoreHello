b593b8fb9fa02208da8f08a069c106ca318ddd8c
WindUpDurb <durbina1991@gmail.com>
2016-12-07 17:44:49 -0800

    Child resources implemented

--- src/CityInfo.API/CitiesDataStore.cs
+++ src/CityInfo.API/CitiesDataStore.cs
@@ -20,19 +20,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York",
-                    Description = "The Big Apple."
+                    Description = "The Big Apple.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "Where the bums at"
+                        },
+                         new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Shoe City",
+                            Description = "Where the shoes at"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Antwerp",
-                    Description = "The one with the cathedral."
+                    Description = "The one with the cathedral.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Dojo",
+                            Description = "Where the karate at"
+                        },
+                         new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Bench",
+                            Description = "Where the sitting at"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "La Puente",
-                    Description = "Fucking Smells"
+                    Description = "Fucking Smells",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Liquor Store",
+                            Description = "Where the dranks at"
+                        },
+                         new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Pizza Loca",
+                            Description = "Where the pizza at"
+                        }
+                    }
                 }
             };
         }
