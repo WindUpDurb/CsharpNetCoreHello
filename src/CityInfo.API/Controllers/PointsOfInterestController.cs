9f87addba7ebc867e6430095c4ed204f7fa10632
WindUpDurb <durbina1991@gmail.com>
2016-12-08 16:03:05 -0800

    POST request

--- src/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ src/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        [HttpGet("{cityId}/pointsofinterest/{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -26,5 +27,31 @@ namespace CityInfo.API.Controllers
             if (pointOfInterest == null) return NotFound();
             return Ok(pointOfInterest);
         }
+
+        [HttpPost("{cityId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if (pointOfInterest == null) return BadRequest();
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null) return NotFound();
+
+            //demo purposes - to be improved
+            var maxPointsOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterst = new PointOfInterestDto()
+            {
+                Id = ++maxPointsOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterst);
+
+            return CreatedAtRoute("GetPointOfInterest", 
+                new { cityId = cityId, id = finalPointOfInterst.Id}, finalPointOfInterst);
+        }
+
     }
 }
