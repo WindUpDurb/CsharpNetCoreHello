b9acb8f3de235da5b310574b061627a12ecf3ec9
WindUpDurb <durbina1991@gmail.com>
2016-12-15 11:03:24 -0800

    Configuring in JSON files

--- src/CityInfo.API/Startup.cs
+++ src/CityInfo.API/Startup.cs
@@ -8,11 +8,37 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using CityInfo.API.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API
 {
     public class Startup
     {
+
+        public static IConfigurationRoot Configuration;
+
+        //Startup constructor
+        //We need to inject the hosting environment,
+        //as we will need that to point the framework
+        //to the root of our application
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                //tell it to find our settings file below:
+                //and we pass in ContentRootPath from hosting env.
+                .SetBasePath(env.ContentRootPath)
+                //optional argument signifies whether or not the appSettings file is optional
+                //reloadOnChange: should the settings file be reloaded if we change it?
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                //The order in which configuration sources is specified is important,
+                //as this establishes the precedence with which settings will be applied
+                //if they exist in multiple locations.
+                //The last configuration specified wins
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional:true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
